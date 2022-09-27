0864f3aeed2029ef3191778ae3d7de1f8a895e17
Luis <lvas248@gmail.com>
2022-09-27 14:50:25 -0400

    commit tree 715398fc25956e8c264bb50353561bce4342fd90

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    self.scan(/[\.|!|?]+/).length
   end
 end
+
+puts "This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
